37cdcc5ff70ddee7d7fb3a2f1efe59f8d55285a6
Danish1342k <119004258+Danish1342k@users.noreply.github.com>
2023-04-19 13:02:50 +0530

    initial commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,14 @@ import { Flex, Text } from '@chakra-ui/react';
 
 function Footer() {
   return (
-    <Flex w="full" color="#adb9c9" display="flex" justifyContent="center">
+    <Flex
+      w="full"
+      color="#adb9c9"
+      display="flex"
+      justifyContent="center"
+      flexDirection={{ base: 'column' }}
+      alignItems="center"
+    >
       <Flex>
         © 2023, Made with ❤️ by
         <Text ml="5px" color="blue.400" fontWeight="600">
